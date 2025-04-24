bbd0eed9e97f7be24fd565ebf62bdac20efe04b2
angel-2003-hub <angel.sebastinfelix@gmail.com>
2025-04-24 09:57:38 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,146 @@
+#program 1
+print("Hello,World!")
+
+#program 2
+print("Angel")
+
+#program 3
+print("Name:Angel")
+print("Age:22")
+
+#program 4
+#String concatination
+first_name="Angel"
+last_name="sebastin"
+print(first_name+' '+last_name)
+#integer
+a=10
+b=20
+print(a+b)
+#float
+c=10.5
+d=3.4
+print(c+d)
+#boolean
+performwell=True
+not_perform=False
+print(performwell or not_performwell)
+
+#program 5
+
+name="angel"
+age="22"
+print(f"Hello,My name is:{name} Iam {age}Years old")
+
+#program 6
+
+name="angel"
+age=22
+print(name)
+print(age)
+
+
+#program 7
+a=10
+b=20
+print(a+b) #Addition
+print(a-b) #subraction
+print(a*b) #Multplication
+print(a/b) #Division
+
+
+#program 8
+
+fruits=['apple','orange','grapes','mango','pine apple']
+for i in fruits:
+    print(i)
+
+
+#program 9
+for i in range(1,1+4):
+    for i in range(1,i+1):
+        print("*",end="")
+    print()
+
+#program 10
+name="angel"
+age=22
+is_student=True
+print("Name:",name)
+print("Age:",age)
+print("is_student:",is_student)
+
+#program 11
+#before modify
+print("Before modify")
+name="angel"
+age=22
+is_student=True
+print("Name:",name)
+print("age:",age)
+print"is_student:",is_student)
+#after modify
+print("after modify")
+name="Angel Sebastin"
+age=age+1
+is_student=False
+print("Name:",name)
+print("age:",age)
+print("is_student:",is_student)
+
+
+#program 12
+
+name="jose"
+age=22
+dob="23-3-2003"
+height=156.7
+weight=47.6
+degree="Mca"
+gender="male"
+print("name:",name)
+print("age:",age)
+print("dob:",dob)
+print("height:",height)
+print("weight:",weight)
+print("degree:",degree)
+print("gender:",gender)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
