399ce633670b5fc23d3cd85a827a718a4426274f
angel-2003-hub <angel.sebastinfelix@gmail.com>
2025-04-24 10:23:25 +0530

    Update practice2.py

--- agile-tribers/sprint1/task2/practice2.py
+++ agile-tribers/sprint1/task2/practice2.py
@@ -1,17 +1,17 @@
 #program 1
-'''userName="sha"
+userName="sha"
 user_id=57344758
 print("camel case:",userName)
-print("snake_case:",user_id)'''
+print("snake_case:",user_id)
 
 #program 2
-'''PI=3.14159
+PI=3.14159
 radius=10
 circle=2*PI*radius
-print("circumstance of the circle:",circle)'''
+print("circumstance of the circle:",circle)
 
 #program 3
-'''list=[3,4,7,6,8]
+list=[3,4,7,6,8]
 print(list[0])
 print(list[4])
 
@@ -22,43 +22,43 @@ list.insert(1,9)
 print(list)      #insert element in the list
 
 list[2]=25
-print(list)     #modify element in the lists'''
+print(list)     #modify element in the lists
 
 #program 4
-'''num1=10
+num1=10
 num2=20
 result=num1+num2
-print(result)'''
+print(result)
 
 #program 5
-'''pi=3.14159
+pi=3.14159
 r=11
 circle=pi*r*r
-print("area of the circle:",circle)'''
+print("area of the circle:",circle)
 
 #program 6
-'''length=int(input("Enter the length:"))
+length=int(input("Enter the length:"))
 width=int(input("Enter the width:"))
 area=length*width
-print("area of the rectangle:",area)'''
+print("area of the rectangle:",area)
 
 #program 7
-'''base=10
+base=10
 height=15
 area=(base*height)/2
-print("Area oh the triangle:",area)'''
+print("Area oh the triangle:",area)
 
 
 #program 8
-'''num1=int(input("Enter the first number:"))
+num1=int(input("Enter the first number:"))
 num2=int(input("Enter the second number:"))
 print("Addition:",num1+num2)
 print("Subraction:",num1-num2)
 print("Multiplication:",num1*num2)
-print("division:",num1/num2)'''
+print("division:",num1/num2)
 
 #program 9
-'''a=10
+a=10
 a+=10
 print("Addition:",a)
 a-=10
@@ -66,34 +66,34 @@ print("Subraction:",a)
 a*=a
 print("Multiplication:",a)
 a/=a
-print("Division:",a)'''
+print("Division:",a)
 
 #program 10
-'''num=20
+num=20
 num+=10
 print(num)
 num-=12
-print(num)'''
+print(num)
 
 #program 11
-'''num1=10
+num1=10
 num2=20
 print("Number1 equal to number2:",num1==num2)
 print("Number1 is not equal to number2:",num1!=num2)
 print("Number1 less than number2:",num1<num2)
 print("Number1 is greater than number2:",num1>num2)
 print("Number1 is less than or equal to number2:",num1<=num2)
-print("Number1 is greater than or equal to number2:",num1>=num2)'''
+print("Number1 is greater than or equal to number2:",num1>=num2)
 
 #program 12
-'''a=True
+a=True
 b=False
 print(a and b)
 print(a or b)
-print(not b)'''
+print(not b)
 
 #program 13
-'''temp=0
+temp=0
 a=10
 b=20
 print("Before swapping")
@@ -108,26 +108,26 @@ print("b:",b)
 print("swapping with using third variable")
 a,b=b,a
 print("a:",a)
-print("b:",b)'''
+print("b:",b)
 
 #program 14
-'''num1=int(input("Enter the first number:"))
+num1=int(input("Enter the first number:"))
 num2=int(input("Enter the second number:"))
 num3=int(input("Enter the third number:"))
 average=(num1+num2+num3)/3
-print(average)'''
+print(average)
 
-'''#program 15
+#program 15
 a=10
 b=30
 c=12
 d=33
 result=(a+b)*c/d
-print(result)'''
+print(result)
 
 
 #program 16
-'''tamil=50
+tamil=50
 english=75
 maths=68
 science=80
@@ -135,9 +135,9 @@ social=86
 total=tamil+english+maths+science+social
 average=total/5
 print("Total:",total)
-print("average:",average)'''
+print("average:",average)
 
-#program 15
+
 
 
 
