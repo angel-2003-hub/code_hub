99c4cfe53f9437516a2aeae9efc609f3d139542a
angel-2003-hub <angel.sebastinfelix@gmail.com>
2025-04-28 15:11:58 +0530

    Update task practice4.py

--- agile-tribers/sprint1/task4/task practice4.py
+++ agile-tribers/sprint1/task4/task practice4.py
@@ -1,44 +1,44 @@
 #program 1
-'''num=int(input("enter the number:"))
+num=int(input("enter the number:"))
 if(num>0):
     print("The number is positive")
 else:
-    print("The number is negative")'''
+    print("The number is negative")
 
 
 #program 2
 
-'''num=int(input("Enter the number:"))
+num=int(input("Enter the number:"))
 if num%2==0:
     print("The number is even")
 else:
-    print("The number is odd") '''
+    print("The number is odd") 
 
 #program 3
-'''base=int(input("Enter the base number:"))
+base=int(input("Enter the base number:"))
 exponent=int(input("Enter the power nunmer:"))
 power=pow(base,exponent)
-print(power)'''
+print(power)
 
 #program 4
-'''num1=int(input("Enter the first number:"))
+num1=int(input("Enter the first number:"))
 num2=int(input("Enter the second number:"))
 if num1==num2:
     print("number1 is equal to number2")
 elif num1>num2:
     print("number1 greater than number2")
 else:
-    print("Number2 is greater than number1")'''
+    print("Number2 is greater than number1")
 
 #program 5
-'''year=int(input("Enter the year:"))
+year=int(input("Enter the year:"))
 if year%4==0:
     print("The year is the leap year")
 else:
-    print("The year is not a leap year")'''
+    print("The year is not a leap year")
 
 #program 6
-'''score=int(input("Enter your score:"))
+score=int(input("Enter your score:"))
 if score>=90:
     print("your grade is A")
 elif score>=80:
@@ -48,10 +48,10 @@ elif score>=70:
 elif score>=60:
     print("your grade is D")
 elif score<60:
-    print("your grade is F")'''
+    print("your grade is F")
 
 #program 7
-'''age=int(input("Enter your age:"))
+age=int(input("Enter your age:"))
 if age<16:
     print("you can't drive")
 elif age>=16 and age<=17:
@@ -59,10 +59,10 @@ elif age>=16 and age<=17:
 elif age>=18 and age<=24:
     print("you can vote but not rent a car")
 elif age>25:
-    print("you can do everything")'''
+    print("you can do everything")
 
 #program 8
-'''for i in range(1,101):
+for i in range(1,101):
     if i%3==0:
         print("Fizz")
     elif i%5==0:
@@ -70,7 +70,7 @@ elif age>25:
     elif i%3==0 and i%5==0:
         print("FizzBuzz")
     else:
-        print(i)'''
+        print(i)
 
 
 #program 9
